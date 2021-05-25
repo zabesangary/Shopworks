6e1f2ba57155d671ae9e488a885bf20d1c89b925
Zabe <zabivirdee@gmail.com>
2021-05-25 21:47:07 +0200

    Update

--- script/script.js
+++ script/script.js
@@ -35,6 +35,6 @@ function toggleText() {
 
                 // Change the text on button
                 // to 'Show Less'
-                buttonText.innerHTML = "Show Less";
+                buttonText.innerHTML = "Show less skills";
             }
         }
