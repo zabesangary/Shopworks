d8741ad7cd2c5d9c85e0aee0302ae2c0df56f55e
Zabe <zabivirdee@gmail.com>
2021-05-25 05:32:22 +0200

    test

---
+++ script/script.js
@@ -0,0 +1,40 @@
+function toggleText() {
+
+            // Get all the elements from the page
+            var points =
+                document.getElementById("points");
+
+            var showMoreText =
+                document.getElementById("moreText");
+
+            var buttonText =
+                document.getElementById("textButton");
+
+
+            if (points.style.display === "none") {
+
+      
+                showMoreText.style.display = "none";
+
+                // Show the dots after the text
+                points.style.display = "inline";
+
+                // Change the text on button to
+                // 'Show More'
+                buttonText.innerHTML = "Show More";
+            }
+
+
+            else {
+
+
+                showMoreText.style.display = "inline";
+
+                // Hide the dots after the text
+                points.style.display = "none";
+
+                // Change the text on button
+                // to 'Show Less'
+                buttonText.innerHTML = "Show Less";
+            }
+        }
